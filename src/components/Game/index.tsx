82bef2958206c53ec299d12c00feb35e7888da76
sky <marcodescrescendo@gmail.com>
2022-03-27 22:41:07 +0200

    moved generateGrid function in grid reducer

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -2,14 +2,10 @@ import Border from "../Border"
 
 const Game = () => {
 
-    const generateGrid = (row:number,column:number) => {//cambiare il nome in quello che fa(in questo caso crea un array bidimensionale)
-        return Array(row).fill(null)
-            .map(()=>Array(column).fill("*"))
-    }
 
     return (
         <div>
-            <Border squares={generateGrid(8,8)}/>
+            <Border />
         </div>
     )
 }
